8161cb2adc5cd8157ce94ba48604028cdff28638
Mark Williams <mwilliams@digitalml.com>
2021-05-23 21:14:35 +0100

    fix: add variable

--- aws/ecs/service/variables.tf
+++ aws/ecs/service/variables.tf
@@ -11,50 +11,50 @@
 
 variable "enabled" {
   description = "Set to false to prevent the module from creating any resources"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "namespace" {
   description = "Namespace, which may relate to the overarching domain or specific business division"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "environment" {
   description = "Describes the environment, e.g. 'PROD', 'staging', 'u', 'dev'"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "name" {
   description = "Identifier for a specific application that may consist of many disparate resources"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "service" {
   description = "Describes an individual microservice running as part of a larger application"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "delimiter" {
   description = "Delimiter to output between 'namespace', 'environment', 'name', 'service' and 'attributes'"
-  type = string
-  default = "-"
+  type        = string
+  default     = "-"
 }
 
 variable "attributes" {
   description = "Any additional miscellaneous attributes to append to the identifier, e.g. 'cluster', 'worker'"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "tags" {
   description = "Additional tags to apply, e.g. '{Owner = 'ABC', Product = 'DEF'}'"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 /* ---------------------------------------------------------------------------------------------------------------------
@@ -63,38 +63,44 @@ variable "tags" {
 
 variable "container_image" {
   description = "Container image to deploy"
-  type = string
+  type        = string
 }
 
 variable "container_memory" {
   description = "The amount of memory (in MiB) to allow the container to use. This is a hard limit, if the container attempts to exceed the container_memory, the container is killed"
-  type = number
+  type        = number
 }
 
 /* ---------------------------------------------------------------------------------------------------------------------
   OPTIONAL VARIABLES
 --------------------------------------------------------------------------------------------------------------------- */
 
+variable "desired_count" {
+  description = "The desired number of services to run"
+  type        = number
+  default     = 1
+}
+
 variable "container_cpu" {
   description = "The number of cpu units to reserve for the container"
-  type = number
-  default = 0
+  type        = number
+  default     = 0
 }
 
 variable "essential" {
   description = "Determines whether all other containers in a task are stopped, if this container fails or stops for any reason"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "container_port" {
   description = "The port number on the container that is bound to the user-specified or automatically assigned host port"
-  type = number
-  default = 80
+  type        = number
+  default     = 80
 }
 
 variable "host_port" {
   description = "The port number on the container instance to reserve for your container"
-  type = number
-  default = 80
+  type        = number
+  default     = 80
 }
